f88d71e2a160b5591d964a13dfe96df9bff6b2f0
tyagian <tyagi.an@husky.neu.edu>
2023-07-23 19:48:55 -0400

    init CI workflow

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -14,12 +14,12 @@ func TestTransferTx(t *testing.T) {
 
 	n := 5
 	amount := int64(10)
-
 	errs := make(chan error)
 	results := make(chan TransferTxResult)
 
 	for i := 0; i < n; i++ {
 		go func() {
+
 			result, err := store.TransferTx(context.Background(), TransferTxParams{
 				FromAccountID: account1.ID,
 				ToAccountID:   account2.ID,
